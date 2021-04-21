beb5167a7814de5a8a98537ca03d10499de38e4c
jgabriel1 <josegabriel@det.ufc.br>
2021-04-21 15:29:13 -0300

    Created header component.

---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Gabriel</Text>
+      </View>
+
+      <Image
+        style={styles.image}
+        source={{ uri: 'https://www.github.com/jgabriel1.png' }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+  },
+
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+
+  userName: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 40,
+  },
+
+  image: {
+    width: 72,
+    height: 72,
+    borderRadius: 40,
+  },
+});
